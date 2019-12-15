4fed00f918f8c8e1f77f8ba65eee932934b0ac32
iamvickyav <vickyavw.10@gmail.com>
2019-12-16 01:21:43 +0530

    rectified equals and hashcode in EmbeddedId

--- modules/domain/src/main/java/com/iamvickyav/RateLimitApi/domain/entity/UserQuotaId.java
+++ modules/domain/src/main/java/com/iamvickyav/RateLimitApi/domain/entity/UserQuotaId.java
@@ -5,29 +5,41 @@ import com.iamvickyav.RateLimitApi.domain.ApiEnum;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 public class UserQuotaId implements Serializable {
 
     @Column(name = "user_id")
-    public Integer userId;
+    public String userId;
 
     @Column(name = "api_id")
     public Integer apiId;
 
     public UserQuotaId(){
-
     }
 
-    public UserQuotaId(Integer userId, Integer apiId) {
+    public UserQuotaId(String userId, Integer apiId) {
         this.userId = userId;
         this.apiId = apiId;
     }
 
     public UserQuotaId(String userId, String apiName) {
-        this.userId = Integer.valueOf(userId);
+        this.userId = userId;
         this.apiId =  ApiEnum.getApiCodeByName(apiName);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserQuotaId that = (UserQuotaId) o;
+        return Objects.equals(userId, that.userId) &&
+                Objects.equals(apiId, that.apiId);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, apiId);
+    }
 }
